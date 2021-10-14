553d289d085a2e0dd0338299a661667dc39d1604
h3nnn4n <uber.renan@gmail.com>
2021-10-14 09:18:40 -0300

    Make the default example a bit better

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 var Rust: any;
 
 const init = () => {
-  let a = Rust.a_plus_b(2, 2);
+  let result = Rust.a_plus_b(2, 2);
+  let text = `In Rust 2 + 2 = ${result}`;
+  let test_works = "<div>If you see this, the wasm chunk was loaded and is working</div>";
 
-  $('#test').text(a);
+  $('#test').text(text);
+  $('#test').after(test_works);
 }
 
 export const load = () => {
